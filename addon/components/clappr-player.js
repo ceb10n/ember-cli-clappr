95c3535ce9340b393b7de76335ec664cfca2648d
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2017-02-09 10:42:10 -0200

    added .js to clappr

---
+++ addon/components/clappr-player.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import layout from '../templates/components/clappr-player';
+import Clappr from '../../bower_components/clappr/dist/clappr.js';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['player'],
+  source: '',
+  didInsertElement() {
+    new Clappr.Player({source: "", parentId: ".player"});
+  }
+});
---
+++ app/components/clappr-player.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-clappr/components/clappr-player';
