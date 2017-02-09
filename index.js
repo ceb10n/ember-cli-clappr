2689f9fb1ba6c6ecd7333024f0149540ca09ec79
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2017-02-09 11:42:12 -0200

    added clappr vendor

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
   name: 'ember-cli-clappr',
 
   included: function(app, parentAddon) {
+    this._super.included(app);
+
     var target = (parentAddon || app);
 
     if (target.app) {
@@ -13,5 +15,11 @@ module.exports = {
 
     var bowerDir = target.bowerDirectory;
     target.import(bowerDir + '/clappr/dist/clappr.js');
+
+    app.import('vendor/clappr.js', {
+      exports: {
+        Clappr: ['default']
+      }
+    });
   }
 };
